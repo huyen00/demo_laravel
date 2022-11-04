3a049ecd89965e629c9f0457fb18bf49f2d8ddb9
Hoàng Thị Huyền <huyenht@VNIMATION.COM>
2022-11-04 13:33:40 +0700

    install inertiaja, vue 2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,25 @@
-require('./bootstrap');
+import './bootstrap';
+
+// import jQuery from 'jQuery'
+
+import Vue from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue'
+import { InertiaProgress } from '@inertiajs/progress'
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+
+createInertiaApp({
+    resolve: name => require(`./Pages/${name}`),
+    setup({ el, App, props, plugin }) {
+        Vue.use(plugin)
+
+        new Vue({
+            render: h => h(App, props),
+        }).$mount(el)
+    },
+})
+Vue.mixin({
+    methods: {
+        route: window.route,
+    }
+})
+InertiaProgress.init({ color: '#4B5563' });
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController1;
-use App\Http\Controllers\CatagoriesController;
-use App\Http\Controllers\Admin\ProductsController;
-use App\Http\Controllers\Admin\SinhvienController;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,81 +15,12 @@ use App\Http\Controllers\Admin\SinhvienController;
 |
 */
 
-
-Route::get('/as',[HomeController1::class, 'index']);
-
-
-// // chuyển hướng bằng controller
-// Route::get('/',[HomeController1::class, 'index']);
-
-// // lấy tham số tự dộng trên url
-// Route::prefix('admin')->group(function(){
-//     Route::get('add/{slug?}-{id?}.html',function($slug=null,$id=null){
-//         $content= 'Thêm sản phẩm:';
-//         $content.='id = '.$id.'<br>';
-//         $content.='slug =' .$slug;
-//         return $content;
-
-//     })->where(
-//         [
-//             'slug'=> '.+',
-//             'id'=> '[0-9]+',
-//         ]
-//         );
-//         // cách đặt tên
-//     Route::get('edit', function(){
-//         return 'Sửa sản phẩm';
-//     })->name('admin.edit');
-//     // middleware: tạo mơi 1 file checkPermission middleware 
-//     Route::prefix('trangchu')->middleware('checkpermission')->group(function(){
-//         Route::get('tin-tuc', function(){
-//             return 'Trang Tin Tức';
-//         });
-//         Route::get('trang-quan-tri', function(){
-//             return ' Trang Quản trị';
-//         });
-//     });
-// });
-
-Route::get('/',function(){
-    return '<h1>Trang chủ Unicode</h1>';
-})->name('home');
-
-// client routes
-Route::prefix('categories')->group(function(){
-
-    // danh sách chuyên mục
-    Route::get('/',[CatagoriesController::class, 'index'])->name('categories.list');
-
-    // lấy chi tiết 1 danh mục ( áp dụng show form sửa chuyên mục)
-    Route::get('/edit/{id}',[CatagoriesController::class, 'getCategory'])->name('categories.edit');
-
-    // xử lý update chuyên mục
-    Route::post('/edit/{id}', [CatagoriesController::class, 'updateCategory']);
-
-    // Hieern thi form add duwx lieeju
-    Route::get('/add', [CatagoriesController::class,'addCategory'])->name('categories.add');
-
-    // xử lý thêm chuyên mục
-    Route::post('/add', [CatagoriesController::class,'handleAddCategory']);
-
-    // Xóa chuyên mục 
-    Route::delete('delete/{id}',  [CatagoriesController::class,'deleteCategory'])->name('categories.delete');
-
-
-
-});
-// admin route
-Route::prefix('admin')->group(function(){
-    Route::resource('products', ProductsController::class);
-    // products- index 
+Route::get('/', function () {
+    return redirect('/login');
 });
 
-Route::prefix('admin')->name('users_sv.')->group(function(){
-    Route::get('/', [SinhvienController::class, 'index'])->name('list');
-    Route::get('add', [SinhvienController::class, 'add'])->name('add');
-    Route::post('add',[SinhvienController::class,'postAdd']);
-    Route::get('/edit/{id}',[SinhvienController::class,'getEdit'])->name('editsv');
-    Route::post('/edit/{id}',[SinhvienController::class,'postEdit'])->name('post-edit');
-   
-});
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
